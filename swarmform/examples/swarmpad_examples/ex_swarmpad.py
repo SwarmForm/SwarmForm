5d64977d08dc12a60a0e67039f4060b8ea4989ef
KalanaDananjaya <kalana.16@cse.mrt.ac.lk>
2020-04-23 09:34:28 +0530

    Add cluster example

--- swarmform/examples/swarmpad_examples/ex_swarmpad.py
+++ swarmform/examples/swarmpad_examples/ex_swarmpad.py
@@ -1,15 +1,22 @@
 from swarmform import SwarmPad
-from swarmform.core.swarmwork import SwarmFlow
 from fireworks import Firework, ScriptTask, FWorker
 from fireworks.core.rocket_launcher import launch_rocket
 
 if __name__ == "__main__":
 	# set up the SwarmPad and reset it
 	swarmpad = SwarmPad()
-	#swarmpad.reset('', require_password=False)
+	swarmpad.reset('', require_password=False)
 
 	# create the Firework consisting of a custom "Addition" task
-	sf = SwarmFlow.from_file('/home/kalana/fyp-new/SwarmForm/swarmform/custom-25-jobs-correct-1587484561/custom-25-jobs-correct.yaml')
+	firework = Firework(ScriptTask.from_str('echo "hello"'))
 
 	# store workflow
-	swarmpad.add_sf(sf)
+	swarmpad.add_sf(firework)
+
+	# Retrieve SwarmFlow from the SwarmPad
+	sf = swarmpad.get_sf_by_id(1)
+
+	sf = swarmpad.get_sf_by_name('Unnamed FW')
+
+	# Run the swarmFlow
+	launch_rocket(swarmpad, FWorker())
