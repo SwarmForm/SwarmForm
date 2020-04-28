71a90b61168c338bad1d191942922f48f4a9d1c7
KalanaDananjaya <kalana.16@cse.mrt.ac.lk>
2020-04-28 14:52:48 +0530

    update task execution time

--- swarmform/util/workflow_generator.py
+++ swarmform/util/workflow_generator.py
@@ -34,7 +34,7 @@ class WorkflowGenerator():
                 # Set execution time as the first element of the firework
                 # Set cores to zero as it is considered only in parallel task clustering
                 # Set empty list to add children
-                firework = [float(child.attrib['runtime']), 0]
+                firework = [float(child.attrib['runtime'])/float(10), 0]
                 workflow_dict.update({fw_id: firework})
 
                 # Create a mapping between dax job id and firework id
